b3a173d1da687b4a679fe822cccd2400c68a8b9d
Renal Apriansyah <renal.apr@gmail.com>
2024-02-24 12:07:56 +0700

    setup ui for categories

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,12 +1,51 @@
-'use client';
+"use client";
 import Layout from "@/components/Layout";
+import {
+  Button,
+  ButtonGroup,
+  Grid,
+  GridItem,
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <Layout>
-      <div style={{ display: 'flex', flex: 11, justifyContent: 'center', alignItems: 'center' }}>
-        <b>Categories</b>
-      </div>
+      <Grid>
+        <GridItem justifySelf='end' colEnd={13} paddingTop={3} paddingRight={3}>
+          <Button colorScheme="blue">Create Category</Button>
+        </GridItem>
+      </Grid>
+      <TableContainer>
+        <Table variant={"simple"}>
+          <TableCaption>Categories</TableCaption>
+          <Thead>
+            <Tr>
+              <Th>ID</Th>
+              <Th>Name</Th>
+              <Th>Actions</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            <Tr>
+              <Th>1</Th>
+              <Th>Category 1</Th>
+              <Th>
+                <ButtonGroup gap={2}>
+                  <Button colorScheme="blue">Edit</Button>
+                  <Button colorScheme="red">Delete</Button>
+                </ButtonGroup>
+              </Th>
+            </Tr>
+          </Tbody>
+        </Table>
+      </TableContainer>
     </Layout>
   );
 }

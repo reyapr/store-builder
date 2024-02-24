6322641ff87993b34614f7bf6e7cf76211791db6
Renal Apriansyah <renal.apr@gmail.com>
2024-02-24 10:49:57 +0700

    refactor to separate function

--- src/app/stores/page.tsx
+++ src/app/stores/page.tsx
@@ -7,22 +7,16 @@ import { useDisclosure, useToast } from "@chakra-ui/react";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { DeleteAlert } from "@/components/DeleteAlert";
+import { useCreateStore } from "@/app/stores/use-create-store";
+import { useUpdateStore } from "@/app/stores/use-update-store";
+import { useDeleteStore } from "@/app/stores/use-delete-store";
 
 const tdStandardStyle = { border: '1px solid', padding: '5px'}
 
 export default function Store() {
   const supabase = createClient();
-  const newFormModal = useDisclosure();
-  const updateFormModal = useDisclosure();
-  const deleteModal = useDisclosure();
-  const [currentEditForm, setCurrentEditForm] = useState({
-    id: '',
-    name: '',
-  } as IUpdateStoreRequest);
-  const [targetDeleteStoreId, setTargetDeleteStoreId] = useState('' as string);
-  
-  const [stores, setStores] = useState([] as any[]);
   const toast = useToast();
+  const [stores, setStores] = useState([] as any[]);
   
   const fetchStores = async () => {
     try {
@@ -40,80 +34,10 @@ export default function Store() {
     }
   }
   
-  const submitNewStore = (request: ISubmitStoreFormRequest) => async () => {
-    const { data } = await supabase.auth.getSession();
-    const user = data?.session?.user;
-    const storRequest = { name: request.name, email: user?.email } as ICreateStoreRequest;
-    
-    try {
-      await axios.post('/api/store', storRequest);
-      fetchStores();
-    } catch (error) {
-      toast({
-        title: "Error",
-        description: (error as Error).message,
-        status: "error",
-        duration: 2500,
-        isClosable: true,
-      })
-      
-    }
-    newFormModal.onClose();
-  }
-  
-  const submitUpdateStore = (request: ISubmitStoreFormRequest) => async () => {
-    try {
-      await axios.patch(`/api/store/${request.id}`, request);
-      fetchStores();
-    } catch (error) {
-      toast({
-        title: "Error",
-        description: (error as Error).message,
-        status: "error",
-        duration: 2500,
-        isClosable: true,
-      })
-    }
-    handleEditClose();
-  }
-  
-  const handleEdit = (request: IUpdateStoreRequest) => {
-    setCurrentEditForm(request);
-    
-    updateFormModal.onOpen();
-  }
-  
-  const handleOpenDeleteModal = (id: string) => {
-    setTargetDeleteStoreId(id);
-    deleteModal.onOpen();
-  }
-  
-  const handleEditClose = () => {
-    setCurrentEditForm({ id: '', name: ''})
-    updateFormModal.onClose();
-  }
-  
-  const handleDeleteClose = () => {
-    setTargetDeleteStoreId('');
-    deleteModal.onClose();
-  }
-  
-  const submitDeleteStore = (id: string) => async () => {
-    try {
-      await axios.delete(`/api/store/${id}`);
-      fetchStores();
-    } catch (error) {
-      toast({
-        title: "Error",
-        description: (error as Error).message,
-        status: "error",
-        duration: 2500,
-        isClosable: true,
-      })
-    }
-    handleDeleteClose();
-  }
-  
+  const createStore = useCreateStore(toast, fetchStores, supabase);
+  const updateStore = useUpdateStore(toast, fetchStores, supabase);
+  const deleteStore = useDeleteStore(toast, fetchStores, supabase);
+
   useEffect(() => {
     fetchStores();
   }, []);
@@ -122,23 +46,23 @@ export default function Store() {
     <Layout>
       <StoreFormModal 
         title="Create new store" 
-        isOpen={newFormModal.isOpen} 
-        onClose={newFormModal.onClose} 
-        onSubmit={submitNewStore} 
+        isOpen={createStore.isOpen} 
+        onClose={createStore.onClose} 
+        onSubmit={createStore.submitNewStore} 
       />
       <StoreFormModal 
         title="Update store" 
-        isOpen={updateFormModal.isOpen} 
-        onClose={handleEditClose} 
-        onSubmit={submitUpdateStore} 
-        data={currentEditForm} 
+        isOpen={updateStore.isOpen} 
+        onClose={updateStore.handleEditClose} 
+        onSubmit={updateStore.submitUpdateStore} 
+        data={updateStore.currentEditForm} 
       />
       <DeleteAlert
-        isOpen={deleteModal.isOpen}
-        onClose={handleDeleteClose}
-        onSubmit={submitDeleteStore}
+        isOpen={deleteStore.isOpen}
+        onClose={deleteStore.handleDeleteClose}
+        onSubmit={deleteStore.submitDeleteStore}
         title="Delete Store"
-        id={targetDeleteStoreId}
+        id={deleteStore.targetDeleteStoreId}
       />
       <div 
         style={{ 
@@ -155,7 +79,7 @@ export default function Store() {
         <div>
           <div>
             <button 
-              onClick={newFormModal.onOpen}  
+              onClick={createStore.onOpen}  
               style={{backgroundColor: '#6a78a6', color: 'white'}}
             >
               Create Store
@@ -175,12 +99,22 @@ export default function Store() {
                   stores.sort((a,b) => a.id > b.id ? -1 : 1).map((item, index) => {
                     return (
                       <tr key={item.id}>
-                        <td  style={tdStandardStyle}>{item.id}</td>
-                        <td  style={tdStandardStyle}>{item.name}</td>
-                        <td  style={tdStandardStyle}>
-                          <button onClick={() => handleEdit({ id: item.id, name: item.name})} style={{backgroundColor: '#6a78a6', color: 'white'}}>Edit</button>
+                        <td style={tdStandardStyle}>{item.id}</td>
+                        <td style={tdStandardStyle}>{item.name}</td>
+                        <td style={tdStandardStyle}>
+                          <button 
+                            onClick={() => updateStore.handleEdit({ id: item.id, name: item.name})} 
+                            style={{backgroundColor: '#6a78a6', color: 'white'}}
+                          >
+                            Edit
+                          </button>
                           |
-                          <button onClick={() => handleOpenDeleteModal(item.id)} style={{backgroundColor: '#6a78a6', color: 'white'}}>Delete</button>
+                          <button 
+                            onClick={() => deleteStore.handleOpenDeleteModal(item.id)} 
+                            style={{backgroundColor: '#6a78a6', color: 'white'}}
+                          >
+                            Delete
+                          </button>
                         </td>
                       </tr>
                     );

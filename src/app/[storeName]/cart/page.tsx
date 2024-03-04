0854043506ff6e43a72de7d8f61391bc71cf718a
Renal Apriansyah <renal.apr@gmail.com>
2024-03-04 16:14:51 +0700

    adjust order position

--- src/app/[storeName]/cart/page.tsx
+++ src/app/[storeName]/cart/page.tsx
@@ -10,7 +10,6 @@ import {
   Button,
   Card,
   CardBody,
-  CardFooter,
   CardHeader,
   Divider,
   Flex,
@@ -20,10 +19,7 @@ import {
   Stack,
   StackDivider,
   Text,
-  Input,
-  FormLabel,
-  FormControl,
-  Textarea
+  SimpleGrid,
 } from "@chakra-ui/react";
 import { clear } from "console";
 import { useState } from "react";
@@ -36,77 +32,86 @@ export default function CartPage({
   const cart = useStore(cartStore, (state) => state, params.storeName);
   const items = (cart.getProducts && cart.getProducts()) || [];
   const totalCartPrice = cart.getTotalPrice && cart.getTotalPrice();
-  
+
   const [input, setInput] = useState({
-    name: '',
-    phone: '',
-    address: ''
-  })
-  
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    name: "",
+    phone: "",
+    address: "",
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
     setInput({
       ...input,
-      [e.target.name]: e.target.value
-    })
-  }
+      [e.target.name]: e.target.value,
+    });
+  };
 
   return (
     <Layout storeName={params.storeName}>
-      <Card>
-        <CardHeader>
-          <Heading>Cart</Heading>
-        </CardHeader>
-
-        <CardBody>
-          <Stack divider={<StackDivider />} spacing="4">
-            {items.map((product) => (
-              <Flex key={product.id}>
-                <Box boxSize={20} marginRight={5}>
-                  <Image
-                    src="https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM="
-                    alt="Green double couch with wooden legs"
-                    sizes="sm"
-                  />
-                </Box>
-                <Box>
-                  <Heading size="md">{product.name}</Heading>
-                  <Text>Price: {toIDRFormat(product.price)}</Text>
-                  <Text>Quantity: {product.quantity}</Text>
-                </Box>
-                <Spacer/>
-                <Box alignSelf='center'>
-                  <NumberInput 
-                    quantity={product.quantity} 
-                    productId={product.id} 
-                    updateProductQuantity={cart.updateProductQuantity}
-                  />
-                </Box>
-              </Flex>
-            ))}
-          </Stack>
-        </CardBody>
-        <Divider />
-        <CardFooter>
-          <Box alignSelf='center'>
-            <Heading fontSize={'xl'}>
-              <Text>Order Detail</Text>
-            </Heading>
-            <Text>Total: {toIDRFormat(totalCartPrice)}</Text>
-          </Box>
-          <Spacer/>
-          <Box alignSelf='center'>
-            <Button bgColor='blue.200'>Pesan</Button>
-          </Box>
-        </CardFooter>
-      </Card>
-      <Card>
-        <CardHeader>
-          <Heading>Data Pemesan</Heading>
-        </CardHeader>
-        <CardBody>
-          <OrdererInput input={input} handleChange={handleChange} />
-        </CardBody>
-      </Card>
+      <SimpleGrid margin={3} spacing={2}>
+        <Card>
+          <CardHeader>
+            <Heading>Cart</Heading>
+          </CardHeader>
+          <Divider color="gray.300" />
+          <CardBody>
+            <Stack divider={<StackDivider />} spacing="4">
+              {items.map((product) => (
+                <Flex key={product.id}>
+                  <Box boxSize={20} marginRight={5}>
+                    <Image
+                      src="https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM="
+                      alt="Green double couch with wooden legs"
+                      sizes="sm"
+                    />
+                  </Box>
+                  <Box>
+                    <Heading size="md">{product.name}</Heading>
+                    <Text>Price: {toIDRFormat(product.price)}</Text>
+                    <Text>Quantity: {product.quantity}</Text>
+                  </Box>
+                  <Spacer />
+                  <Box alignSelf="center">
+                    <NumberInput
+                      quantity={product.quantity}
+                      productId={product.id}
+                      updateProductQuantity={cart.updateProductQuantity}
+                    />
+                  </Box>
+                </Flex>
+              ))}
+            </Stack>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading>Data Pemesan</Heading>
+          </CardHeader>
+          <Divider color="gray.300" />
+          <CardBody>
+            <OrdererInput input={input} handleChange={handleChange} />
+          </CardBody>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading>Order Summary</Heading>
+          </CardHeader>
+          <Divider color="gray.300" />
+          <CardBody>
+            <Flex>
+              <Box alignSelf="center">
+                <Text>Total: {toIDRFormat(totalCartPrice)}</Text>
+              </Box>
+              <Spacer />
+              <Box alignSelf="center">
+                <Button bgColor="blue.200">Pesan</Button>
+              </Box>
+            </Flex>
+          </CardBody>
+        </Card>
+      </SimpleGrid>
     </Layout>
   );
 }

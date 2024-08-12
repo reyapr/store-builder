084ace4aab2305b0ecff9a937135cfe6322766ed
Rifki Fauzi <kubido@gmail.com>
2024-08-12 14:52:37 +0700

    fix add to cart

--- src/stores/useCart.ts
+++ src/stores/useCart.ts
@@ -4,14 +4,14 @@ import { persist } from 'zustand/middleware'
 
 import { IProduct, IProductCart } from '@/interfaces/product'
 
-export interface IState {
+interface CartState {
   products: IProductCart[]
 }
 
-export interface IActions {
+interface CartActions {
   addProduct: (product: IProduct) => void
   removeProduct: (productId: string) => void
-  reduceQuantity: (productId: string) => void,
+  reduceQuantity: (productId: string) => void
   getTotalQuantity: (productId?: string) => number
   clearCart: () => void
   getProducts: () => IProductCart[]
@@ -19,66 +19,55 @@ export interface IActions {
   updateProductQuantity: (productId: string, num: number) => void
 }
 
-export const cartStore = create<IState & IActions>()(
+export const cartStore = create<CartState & CartActions>()(
   persist(
     (set, get) => ({
       products: [],
-      addProduct: (product) => {
-        const productInCart = get().products.find((p) => p.id === product.id)
 
-        if (productInCart) {
-          set((state) => ({
-            products: state.products.map((p) =>
+      addProduct: (product) => set((state) => {
+        const existingProduct = state.products.find(p => p.id === product.id)
+        if (existingProduct) {
+          return {
+            products: state.products.map(p => 
               p.id === product.id ? { ...p, quantity: p.quantity + 1 } : p
             )
-          }))
-        } else {
-          set((state) => ({
-            products: [
-              ...state.products,
-              {
-                ...product,
-                quantity: 1
-              }
-            ]
-          }))
-        }
-      },
-      removeProduct: (productId) =>
-        set((state) => ({
-          products: state.products.filter((product) => product.id !== productId)
-        })),
-      getTotalQuantity: (productId) => {
-        let products = get().products;
-        if(productId){
-          products = products.filter(product => product.id === productId)
-        }
-        
-        return products?.reduce((acc, product) => acc + product.quantity, 0)
-      },
-      reduceQuantity: (productId) => {
-        set((state) => ({
-          products: state.products.map((p) => {
-            const isMinus = p.quantity - 1 < 0
-            return p.id === productId !== isMinus ? { ...p, quantity: p.quantity - 1 } : p
           }
-          )
-        }))
-      }, 
+        }
+        return { products: [...state.products, { ...product, quantity: 1 }] }
+      }),
+
+      removeProduct: (productId) => set((state) => ({
+        products: state.products.filter(p => p.id !== productId)
+      })),
+
+      getTotalQuantity: (productId) => {
+        const products = productId 
+          ? get().products.filter(p => p.id === productId)
+          : get().products
+        return products.reduce((acc, p) => acc + p.quantity, 0)
+      },
+
+      reduceQuantity: (productId) => set((state) => ({
+        products: state.products.map(p => 
+          p.id === productId && p.quantity > 0 
+            ? { ...p, quantity: p.quantity - 1 } 
+            : p
+        )
+      })),
+
       clearCart: () => set({ products: [] }),
+
       getProducts: () => get().products,
-      getTotalPrice: () =>
-        get().products.reduce(
-          (acc, product) => acc + product.price * product.quantity,
-          0
-        ),
-      updateProductQuantity: (productId, num) => {
-        set((state) => ({
-          products: state.products.map((p: IProductCart) =>
-            p.id === productId ? { ...p, quantity: num } : p
-          )
-        }))
-      }
+
+      getTotalPrice: () => get().products.reduce(
+        (acc, p) => acc + p.price * p.quantity, 0
+      ),
+
+      updateProductQuantity: (productId, num) => set((state) => ({
+        products: state.products.map(p => 
+          p.id === productId ? { ...p, quantity: num } : p
+        )
+      }))
     }),
     {
       name: 'cart'
